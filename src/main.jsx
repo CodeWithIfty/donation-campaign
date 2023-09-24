648ba41161cf6abaeafd7f2beb2b000b01892fa1
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-09-24 20:16:04 +0600

    created react project --> install react router, tailwind, daisyUI --> create some component and set some layout --> Lets begin

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Home from './components/Home/Home';
+import Root from './components/Root/Root';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+<RouterProvider router={router} />
+  </React.StrictMode>,
+)
