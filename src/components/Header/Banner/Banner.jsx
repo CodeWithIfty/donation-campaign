07d3a335c04d7ae9f0bf01554e7909ca776b664a
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-09-26 19:47:47 +0600

    added category search funcationality

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,16 +1,43 @@
+import  { useState } from 'react';
+import PropTypes from 'prop-types'
 
-const Banner = () => {
+const Banner = ({ onCategoryChange }) => {
+  const [categoryInput, setCategoryInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setCategoryInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onCategoryChange(categoryInput);
+  };
+
   return (
-    <div className="md:h-[70vh] h-[50vh]  bg-[url('../src/assets/bannerImg.jpeg')] bg-cover bg-center bg-no-repeat bg-blend-lighten bg-white bg-opacity-90 flex justify-center items-center">
-      <div className=" mt-28 md:mt-0">
-        <h1 className="lg:text-5xl md:text-4xl font-bold text-2xl mb-12 text-center drop-shadow-2xl ease-out duration-1000 hover:scale-105 ">I Grow By Helping People In Need</h1>
-        <div className=" flex items-center w-full justify-center focus:scale-105 ease-in duration-100 ">
-        <input type="text"  className="py-3 md:w-96 rounded-s-lg outline-none p-4 focus:drop-shadow-2xl shadow bg-white border-2  ease-in duration-100" placeholder="Search here...."/>
-        <button className="bg-[#FF444A] py-3 rounded-e-lg px-6 font-bold ease-out duration-100 hover:scale-105  text-white shadow-md border-2 border-[#FF444A]">Search</button>
+    <div className="md:h-[70vh] h-[50vh] bg-[url('../src/assets/bannerImg.jpeg')] bg-cover bg-center bg-no-repeat bg-blend-lighten bg-white bg-opacity-90 flex justify-center items-center">
+      <div className="mt-28 md:mt-0">
+        <h1 className="lg:text-5xl md:text-4xl font-bold text-2xl mb-12 text-center drop-shadow-2xl ease-out duration-1000 hover:scale-105">I Grow By Helping People In Need</h1>
+        <div className="flex items-center w-full justify-center focus:scale-105 ease-in duration-100">
+          <input
+            type="text"
+            className="py-3 md:w-96 rounded-s-lg outline-none p-4 focus:drop-shadow-2xl shadow bg-white border-2 ease-in duration-100"
+            placeholder="Search here...."
+            value={categoryInput}
+            onChange={handleInputChange}
+          />
+          <button
+            className="bg-[#FF444A] py-3 rounded-e-lg px-6 font-bold ease-out duration-100 hover:scale-105 text-white shadow-md border-2 border-[#FF444A]"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
+};
+
+Banner.propTypes ={
+  onCategoryChange: PropTypes.func
 }
 
-export default Banner
+export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,37 @@
-import Card from "../Card/Card"
-import Banner from "../Header/Banner/Banner"
-import { useEffect, useState } from "react"
+import Card from "../Card/Card";
+import Banner from "../Header/Banner/Banner";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   const [donations, setDonations] = useState([]);
+  const [filteredDonations, setFilteredDonations] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch('donation.json')
-    .then(res => res.json())
-    .then(data => setDonations(data))
-  },[])
+      .then(res => res.json())
+      .then(data => {
+        setDonations(data);
+        setFilteredDonations(data); // Initialize filtered donations with all donations
+      });
+  }, []);
+
+  const handleCategoryFilter = (category) => {
+    const filtered = donations.filter(donation => {
+      return donation.category.toLowerCase().includes(category.toLowerCase());
+    });
+    setFilteredDonations(filtered);
+  };
+
   return (
     <div>
-        <Banner/>
-      <div className=" container mx-auto py-16 flex justify-evenly flex-wrap gap-7">
+      <Banner onCategoryChange={handleCategoryFilter} />
+      <div className="container mx-auto py-16 flex justify-evenly flex-wrap gap-7">
         {
-          donations?.map((donation => <Card key={donation.id} donation={donation}/>))
+          filteredDonations.map(donation => <Card key={donation.id} donation={donation} />)
         }
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
